7f3ee8448dfa3dda3af81ddd664fb57710e6ed55
ceva nume <sadagain>
2021-10-11 16:00:45 +0300

    facut si pb 3

--- main2.py
+++ main2.py
@@ -3,7 +3,8 @@ def afisare_meniu ():
     print("1.Citeste datele ")
     print("2.Determinare cea mai lungă subsecvență cu proprietatea ca suma numerelor este număr prim ")
     print("3.Determinare cea mai lungă subsecvență cu proprietatea ca numărul de cifre este în ordine descrescătoare.")
-    print("4.Iesie ")
+    print("4.Determinare cea mai lungă subsecvență cu proprietatea ca numărerele sa fie palindrome.")
+    print("5.Iesie ")
 
 
 def citire_date_problema():
@@ -102,7 +103,63 @@ def test_get_longest_digit_count_desc():
     assert get_longest_digit_count_desc([543,4,3,123])==[543,4,3]
 
 
+def is_palindrom(x):
+    """
+    Verificam daca un numar este palindrom
+    :param x: Numarul pe care il verificam
+    :return: True in caz ca e palinfrom false in caz contrar
+    """
+    copie=x
+    y=0
+    while copie!=0:
+        y=copie%10+y*10
+        copie=copie//10
+    if y==x:
+        return True
+    return False
+
+def are_all_palindrom(l):
+    """
+    Verifica daca toate numerele dintr-o lista sunt palindroame
+    :param lst:
+    :return:True daca toate numerele din lista sunt palindroame, False in caz contrar
+    """
+    for x in l:
+        if not is_palindrom(x):
+            return False
+    return True
+
+
+def test_are_all_palindromes():
+    assert are_all_palindrom([121,232]) is True
+
+def test_is_palindrom():
+    assert is_palindrom(123) is False
+    assert is_palindrom(121) is True
+
+test_is_palindrom()
+
+
+def get_longest_all_palindromes(l):
+    """
+    Returnam ea mai lunga secventa cu propietatea ca numerele sunt palindroame
+    :param l: Lista verificata
+    :return: Cea mai lunga seccventa
+    """
+    lungMaxDesc = []
+    for i in range(len(l)):
+        for j in range(len(l)):
+            if are_all_palindrom(l[i:j+1]) and len(lungMaxDesc)<len(l[i:j+1]):
+                lungMaxDesc=l[i:j+1]
+    return lungMaxDesc
+
+def test_get_longest_all_programes():
+    assert get_longest_all_palindromes([121,123321,12])==[121,123321]
+    assert get_longest_all_palindromes([12,21])==[]
+    assert get_longest_all_palindromes([121,312,23])==[121]
+
 def main():
+    test_get_longest_all_programes()
     test_numerele_sunt_descrescatoare()
     test_get_longest_digit_count_desc()
     l = []
@@ -116,6 +173,8 @@ def main():
         elif optiune == "3":
             print(get_longest_digit_count_desc(l))
         elif optiune=="4":
+            print(get_longest_all_palindromes(l))
+        elif optiune=="5":
             break
         else:
             print("Optiune gresita! Reincercati!")
